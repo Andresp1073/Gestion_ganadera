acbd41138fdf006c33b5f01b865470aadd2255b0
Lilibeth Anacona <andreaanacona469@gmail.com>
2025-03-28 21:22:10 -0500

    Se crean las capas de model, logic y  data

---
+++ RanchMasterWebApp/Model/LocationsEntity.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model;
+
+public class LocationsEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id_locations")]
+    public int IdLocations { get; set; }
+    
+    [Required]
+    [StringLength(45)]
+    public string Name { get; set; }
+    
+    [Required]
+    public int Capacity { get; set; }
+    
+}
--- RanchMasterWebApp/Model/obj/Debug/net9.0/Model.AssemblyInfo.cs
+++ RanchMasterWebApp/Model/obj/Debug/net9.0/Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440a15eb1818a725d4c8087d17083ca237b167a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5943f3799cffcfae4b9f764a31e8e1f748fc226e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
