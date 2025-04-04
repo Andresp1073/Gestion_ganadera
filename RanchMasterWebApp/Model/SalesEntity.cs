d77aa71ce395f3bbb9e6418d5c83fba4b0e285c0
juan miguel <juanmiguelcedenosolano@gmail.com>
2025-04-04 12:22:42 -0500

    Se creo clase Sales

---
+++ RanchMasterWebApp/Model/SalesEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model;
+
+public class SalesEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int IdSale { get; set; }
+   
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime SaleDateTime { get; set; }
+   
+    //llave foranea
+    public string IdBuyer { get; set; }
+    public BuyersEntity Buyer { get; set; }
+   
+   
+   
+    public string IdAnimal { get; set; }
+    public AnimalsEntity Animals { get; set; }
+
+}
