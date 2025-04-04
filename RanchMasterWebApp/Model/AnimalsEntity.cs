ac92d647f46b5247277f8afe0dbf448b8942bc12
juan miguel <juanmiguelcedenosolano@gmail.com>
2025-04-03 21:55:14 -0500

    Se creo la clase AnimalsEntity

---
+++ RanchMasterWebApp/Model/AnimalsEntity.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Model;
+
+public class AnimalsEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int IdAnimal { get; set; }
+
+    [Required]
+    [Column(TypeName = "char(1)")]
+    public char Gender { get; set; }
+    
+    [Required]
+    public int Age { get; set; }
+    
+    
+    [Required]
+    [StringLength(8)]
+    public string Status { get; set; }
+    
+    // Claves foráneas
+    public string IdLocation { get; set; }
+    public LocationsEntity Location { get; set; }
+
+    
+    public string IdSchedule { get; set; }
+    public ScheduleEntity Schedule { get; set; }
+
+
+    
+    public string IdReproduction { get; set; }
+    public ReproductionsEntity  Reproduction { get; set; }
+
+    
+    public string IdRace  { get; set; }
+    public RaceEntity Race { get; set; }
+    
+    //Lista
+    
+    public ICollection<SalesEntity> Sales { get; set; }
+
+}
