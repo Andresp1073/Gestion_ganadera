89f8729348cbdb201e5fc1a329708267c2cddfe6
Lilibeth Anacona <andreaanacona469@gmail.com>
2025-04-04 20:13:02 -0500

    Se Actualiza la clase FeedingSchedulesEntity

--- RanchMasterWebApp/Model/FeedingSchedulesEntity.cs
+++ RanchMasterWebApp/Model/FeedingSchedulesEntity.cs
@@ -7,18 +7,17 @@ public class FeedingSchedulesEntity
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int  IdSchedule { get; set; }
-    
+    public int IdSchedule { get; set; }
+
     [Required]
-    [Column(TypeName = "DATE")]
-    public DateOnly FeedingTime { get; set; }
+    public DateTime FeedingTime { get; set; }
     
-    [Required]
     public int IdFeedingDetail { get; set; }
     
-    [ForeignKey("IdFeedingDetails")]
-    public FeedingDetailEntity FeedingDetail { get; set; }
-    
-    public ICollection<AnimalsEntity> Animals { get; set; }
+    //lave foranea
+    [ForeignKey("IdFeedingDetail")]
+    public FeedingDetailsEntity FeedingDetail { get; set; }
 
+    public ICollection<AnimalsEntity> Animals { get; set; }
+    
 }
