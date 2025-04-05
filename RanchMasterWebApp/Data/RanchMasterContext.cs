fffb209df76283dadf9ba9782c258c8c521cf76a
Lilibeth Anacona <andreaanacona469@gmail.com>
2025-04-04 19:19:58 -0500

    Se crea el RanchMasterContext

---
+++ RanchMasterWebApp/Data/RanchMasterContext.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Data;
+
+public class RanchMasterContext : DbContext
+{
+    public DbSet<AnimalsEntity> Animals { get; set; }
+    public DbSet<BuyersEntity> Buyers { get; set; }
+    public DbSet<FeedingDetailsEntity> FeedingDetails { get; set; }
+    public DbSet<FeedingSchedulesEntity> FeedingSchedules { get; set; }
+    public DbSet<HealthsRecordsEntity> HealthsRecords { get; set; }
+    public DbSet<LocationsEntity> Locations { get; set; }
+    public DbSet<RacesEntity> Races { get; set; }
+    public DbSet<ReproductionsEntity> Reproductions { get; set; }
+    public DbSet<SalesEntity> Sales { get; set; }
+    public DbSet<VeterinariansEntity> Veterinarians { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(
+            connectionString: "server=localhost; user=root; password=Root123!; port=3306; database=RanchMasterDB;",
+            new MySqlServerVersion(new Version(8, 0, 34))
+        );
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<RacesEntity>().HasData(
+            new RacesEntity { IdRace  = 1, Name = "Cebu", Description = "Pequeña" }
+        );
+
+    }
+}    
+
+
+    
+
+
+    
+        
+        
+    
