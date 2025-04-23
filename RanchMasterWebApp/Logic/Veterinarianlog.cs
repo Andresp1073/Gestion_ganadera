7e43224dfa00f8c17106c6a6b89d7c95c87c60b4
juan miguel <juanmiguelcedenosolano@gmail.com>
2025-04-23 16:20:37 -0500

    Se creo la clase LocationLog

---
+++ RanchMasterWebApp/Logic/Veterinarianlog.cs
@@ -0,0 +1,32 @@
+﻿using Data;
+using Model;
+
+namespace Logic;
+
+public class Veterinarianlog
+{
+    public List<VeterinariansEntity> VeterinarianList()
+    {
+        using (var db = new RanchMasterContext())
+        {
+            return db.Veterinarians.ToList();
+        }
+    }
+
+    public void Update(VeterinariansEntity objveterinarian)
+    {
+        using (var db = new RanchMasterContext())
+        {
+            db.Veterinarians.Update(objveterinarian);
+            db.SaveChanges();
+        }
+    }
+    public void CreateVeterinarian(VeterinariansEntity objveterinarian)
+    {
+        using (var db = new RanchMasterContext())
+        {
+            db.Veterinarians.Add(objveterinarian);
+            db.SaveChanges();
+        }
+    }
+}
