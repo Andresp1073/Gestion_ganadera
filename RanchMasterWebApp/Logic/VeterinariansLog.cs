f17e7790803552996e8b057c87d82684cbd3f5fe
Andres Mauricio Peña <andresmauriciope1073@gmail.com>
2025-05-20 13:53:09 -0500

    Se modifico VeterinariansLog.cs

--- RanchMasterWebApp/Logic/VeterinariansLog.cs
+++ RanchMasterWebApp/Logic/VeterinariansLog.cs
@@ -5,7 +5,7 @@ namespace Logic;
 
 public static class VeterinariansLog
 {
-    public static List<VeterinariansEntity> VeterinarianList()
+    public List<VeterinariansEntity> VeterinarianList()
     {
         using (var db = new RanchMasterContext())
         {
@@ -13,7 +13,7 @@ public static class VeterinariansLog
         }
     }
 
-    public static void UpdateVeterinarians(VeterinariansEntity objveterinarian)
+    public void UpdateVeterinarians(VeterinariansEntity objveterinarian)
     {
         using (var db = new RanchMasterContext())
         {
