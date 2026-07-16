31d3b799ae5e4c41ba33776168f73b55c85ca8aa
Andresp1073 <andresmauriciope1073@gmail.com>
2026-07-16 15:28:22 -0500

    Read MySQL connection string from env var with fallback

--- RanchMasterWebApp/Data/RanchMasterContext.cs
+++ RanchMasterWebApp/Data/RanchMasterContext.cs
@@ -31,8 +31,10 @@ public class RanchMasterContext : IdentityDbContext<ApplicationUser>
     {
         if (!optionsBuilder.IsConfigured)
         {
+            var connStr = Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING")
+                ?? "server=localhost;user=root;password=1234567890;port=3306;database=RanchMasterDB;";
             optionsBuilder.UseMySql(
-                "server=localhost;user=root;password=1234567890;port=3306;database=RanchMasterDB;",
+                connStr,
                 new MySqlServerVersion(new Version(8, 0, 34))
             );
         }
